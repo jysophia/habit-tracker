2d6de62b1fd709ee929b9448911a7b7031a4499e
William <wgumboc@gmail.com>
2023-03-25 20:11:47 -0700

    Controller setup

--- back-end/controllers/habitController.js
+++ back-end/controllers/habitController.js
@@ -1,30 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get habits
 // @route GET /api/habits
 // @access Private
-const getHabits = (req, res) => {
+const getHabits = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get habits'})
-}
+})
 
 // @desc Set habit
 // @route POST /api/habits
 // @access Private
-const setHabit = (req, res) => {
+const setHabit = asyncHandler(async (req, res) => {
+    if(!req.body.text) {
+      res.status(400)
+      throw new Error('Text field missing')
+    }
+
     res.status(200).json({ message: 'Set habit'})
-}
+})
 
 // @desc Update habit
 // @route PUT /api/habits/:id
 // @access Private
-const updateHabit = (req, res) => {
+const updateHabit = asyncHandler(async (req, res) => {
     res.status(200).json({ test: `Update habit ${req.params.id}` })
-}
+})
 
 // @desc Get habits
 // @route DELETE /api/habits/:id
 // @access Private
-const deleteHabit = (req, res) => {
+const deleteHabit = asyncHandler(async (req, res) => {
     res.status(200).json({ test: `Delete habit ${req.params.id}` })
-}
+})
 
 module.exports = {
     getHabits,
